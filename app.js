1563714cc18499a579fbf12ad78b5fda96618bbf
Andrew Perotti <Perotti28@gmail.com>
2026-02-24 10:01:28 -0800

    feat: add middleware to parse input data
    docs: add comments to describe middleware

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const ejs = require("ejs");
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 
-//set the engine and define the views location
+//set the view engine and define the views location
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//use a specific router for specific route, here just a router for any root prefacing routes
 app.use("/", indexRouter);
 
+//middleware to parse form input entry data into the request
+app.use(express.urlencoded({ extended: true }));
+
+//set the server to run on port 3001
 app.listen(3001);

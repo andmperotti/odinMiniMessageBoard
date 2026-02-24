f1d50dae2c77391f750f09aa1f7ff59677aa9789
Andrew Perotti <Perotti28@gmail.com>
2026-02-24 09:19:11 -0800

    build: import routers and set them to be used

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
 //set the engine and define the views location
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 app.listen(3001);

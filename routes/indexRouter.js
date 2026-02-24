804ee5dd0c40636b02ecc1520953bfcab204603c
Andrew Perotti <Perotti28@gmail.com>
2026-02-24 12:09:20 -0800

    feat: add index property to new message objects
    feat: add new route for individual message details
    
    feat: add catch all for non existent routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,11 +6,13 @@ const messages = [
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    id: 0,
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    id: 1,
   },
 ];
 
@@ -22,13 +24,21 @@ indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
 indexRouter.post("/new", (req, res) => {
-  console.log(req.body);
   messages.push({
     text: req.body.messageText,
     user: req.body.authorsName,
     added: new Date(),
+    id: messages.length,
   });
   res.redirect("/");
 });
 
+indexRouter.get("/messages/:messageId", (req, res) => {
+  res.render("message", { message: messages[req.params.messageId] });
+});
+
+//catch all route to redirect users back to main page if they request something not set up or that they're not privileged to see
+indexRouter.all("/*splash", (req, res) => {
+  res.redirect("/");
+});
 module.exports = indexRouter;

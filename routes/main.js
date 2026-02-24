9875a0065df5d7a51a16b1b69f081735b5957176
Andrew Perotti <Perotti28@gmail.com>
2026-02-24 08:58:20 -0800

    feat: add messages data to indexRouter

---
+++ routes/main.js
@@ -0,0 +1,15 @@
+const router = require("express").router;
+indexRouter = router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
